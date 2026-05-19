0ced976149ec0846be6be2269da5803d2f21d43a
RBZ-21 <rdbrown211@gmail.com>
2026-05-19 18:27:19 -0400

    Embed Statuspage incident banner

--- frontend-v2/src/vite-env.d.ts
+++ frontend-v2/src/vite-env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_APP_VERSION?: string;
   readonly VITE_SENTRY_DSN?: string;
+  readonly VITE_STATUSPAGE_API_URL?: string;
+  readonly VITE_STATUSPAGE_URL?: string;
 }
 
 interface ImportMeta {
